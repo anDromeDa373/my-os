5e607c1f6872e7f5d517fb6c39eaba1e3aacd224
anDromeDa373 <koshikeyu1462@icloud.com>
2026-03-17 14:18:27 +0900

    ms_abiで取得するようにした

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
-// main.c
-// UEFI（PCの起動システム）がこの関数を最初に呼び出します
-void efi_main(void* image_handle, void* system_table) {
-    // UEFI System Tableという「道具箱」から、文字出力機能の住所を計算します
-    // 構造体の細かい定義を省くためのハック的な書き方です
+// UEFIから呼ばれるエントリーポイント。ここもMS ABIで受け取る必要がある。
+void __attribute__((ms_abi)) efi_main(void* image_handle, void* system_table) {
     struct {
         char _padding[64];
         struct {
             void* _unused;
-            long long (*output_string)(void*, unsigned short*);
+            // output_stringを呼ぶ際、RDI/RSIではなくRCX/RDXレジスタを使うよう明示
+            unsigned long long (__attribute__((ms_abi)) *output_string)(void*, unsigned short*);
         } *con_out;
     } *st = system_table;
 
-    // 画面に文字を出します。L"..." はUEFIが要求する16bit文字形式です
+    // L"..." (16bitワイド文字) で文字列を渡す
     st->con_out->output_string(st->con_out, (unsigned short*)L"Hello from Bare Metal!\r\n");
 
-    // 止まらないとすぐにPCが再起動してしまうので、無限ループさせます
+    // UEFIに制御を戻さずフリーズさせる
     while (1);
 }
